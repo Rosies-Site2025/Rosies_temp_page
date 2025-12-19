3ffebebfd5059d6d0af3d0bab38bb667e8c5280f
Rosies-Site2025 <fatimaespinoza4@gmail.com>
2025-12-18 22:13:11 -0800

    Bots

---
+++ robots.ts
@@ -0,0 +1,22 @@
+import type { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: [
+          '/api/',
+          '/_next/',
+          '/admin',
+          '/login',
+          '/register',
+          '/forgot-password',
+        ],
+      },
+    ],
+    sitemap: 'https://www.rosiesjanitorialoc.com/sitemap.xml',
+    host: 'https://www.rosiesjanitorialoc.com',
+  };
+}
